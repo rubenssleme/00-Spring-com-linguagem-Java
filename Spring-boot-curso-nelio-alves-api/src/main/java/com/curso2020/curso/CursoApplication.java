fd21b157c382d993d1817cefa2033c912c1e5d65
rubens leme <rubenss.leme@gmail.com>
2022-05-18 11:56:09 -0300

    ajuste vscode

--- Spring-boot-curso-nelio-alves-api/src/main/java/com/curso2020/curso/CursoApplication.java
+++ Spring-boot-curso-nelio-alves-api/src/main/java/com/curso2020/curso/CursoApplication.java
@@ -2,7 +2,7 @@ package com.curso2020.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Ajuste 
 @SpringBootApplication
 public class CursoApplication {
 
